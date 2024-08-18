d8dabebe05d81749ee7444c3baebe4322316f367
victor <victor.h.gutierrez.carvalho>
2024-08-18 01:25:51 -0300

    setimo commit

---
+++ js/services.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuLinks = document.querySelectorAll('.menu ul li a');
+    const overlay = document.querySelector('.overlay');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Previne o redirecionamento imediato
+            const targetUrl = this.href; // Captura o link de destino
+
+            overlay.classList.add('active'); // Ativa a animação
+
+            // Espera a animação terminar antes de redirecionar
+            setTimeout(() => {
+                window.location.href = targetUrl;
+            }, 700); // 700ms deve coincidir com a duração da animação
+        });
+    });
+});
+
+// Menu toggle
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+const menuClose = document.getElementById('menu-close');
+
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('menu-active');
+});
+
+menuClose.addEventListener('click', () => {
+    menu.classList.remove('menu-active');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+    
+    setTimeout(() => {
+        fadeInElements.forEach(el => {
+            el.classList.add('active');
+        });
+    }, 300); // Ajuste o tempo para um delay de aparição, se necessário
+});
+
